c02b3dd85572a83596f77c0367d6c381f7260fb0
Persolute <1538520381@qq.com>
2024-02-15 14:17:20 +0800

    Main.java

---
+++ src/study/Component/Frame.java
@@ -0,0 +1,11 @@
+package study.Component;
+
+/**
+ * @author Persolute
+ * @version 1.0
+ * @description 窗体
+ * @email 1538520381@qq.com
+ * @date 2024/2/15 14:16
+ */
+public class Frame {
+}
---
+++ src/study/Main.java
@@ -0,0 +1,16 @@
+package study;
+
+import study.Component.Frame;
+
+/**
+ * @author Persolute
+ * @version 1.0
+ * @description 启动类
+ * @email 1538520381@qq.com
+ * @date 2024/2/15 14:15
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Frame();
+    }
+}
