00ecf778d121f3a8bc4bf959cc6d24e4bb513948
Persolute <1538520381@qq.com>
2024-02-15 14:29:27 +0800

    创建窗体

---
+++ src/study/Parameter.java
@@ -0,0 +1,13 @@
+package study;
+
+/**
+ * @author Persolute
+ * @version 1.0
+ * @description 参数
+ * @email 1538520381@qq.com
+ * @date 2024/2/15 14:18
+ */
+public class Parameter {
+    // 窗体宽度
+    public static final Integer FRAMEWIDTH = 400;
+}
