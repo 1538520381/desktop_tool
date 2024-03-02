a5b1560688531c2fea8a657b3d225a102712dec1
Persolute <1538520381@qq.com>
2024-03-02 19:55:31 +0800

    提取出账单模块操作区表单父类

--- src/study/component/tab/account/operation/date/Date.java
+++ src/study/component/tab/account/operation/date/Date.java
@@ -1,20 +1,13 @@
 package study.component.tab.account.operation.date;
 
-import study.Parameter;
 import study.component.Frame;
 import study.component.tab.account.operation.date.day.Day;
 import study.component.tab.account.operation.date.dayLabel.DayLabel;
-import study.component.tab.account.operation.date.label.Label;
 import study.component.tab.account.operation.date.month.Month;
 import study.component.tab.account.operation.date.monthLabel.MonthLabel;
 import study.component.tab.account.operation.date.year.Year;
 import study.component.tab.account.operation.date.yearLabel.YearLabel;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import study.component.tab.account.operation.father.ItemFather;
 
 /**
  * @author Persolute
@@ -23,22 +16,15 @@ import java.awt.event.MouseEvent;
  * @email 1538520381@qq.com
  * @date 2024/3/1 16:55
  */
-public class Date extends JPanel {
-    private final Frame frame;
-
+public class Date extends ItemFather {
     public Date(Frame frame) {
-        this.frame = frame;
-
-        setPreferredSize(new Dimension(Parameter.FRAME_WIDTH / 2, Parameter.ACCOUNT_OPERATION_ITEM_HEIGHT));
+        super(frame, "日期");
 
         addComponent();
         addListener();
     }
 
     private void addComponent() {
-        setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-
-        add(new Label(frame));
         add(new Year(frame));
         add(new YearLabel(frame));
         add(new Month(frame));
@@ -48,16 +34,6 @@ public class Date extends JPanel {
     }
 
     private void addListener() {
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                frame.mouseEntered();
-            }
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                frame.mouseExited(e.getX() + getLocationOnScreen().x - frame.getX(), e.getY() + getLocationOnScreen().y - frame.getY());
-            }
-        });
     }
 }
--- src/study/component/tab/account/operation/father/ItemFather.java
+++ src/study/component/tab/account/operation/father/ItemFather.java
@@ -1,7 +1,8 @@
-package study.component.tab.account.operation.date.label;
+package study.component.tab.account.operation.father;
 
 import study.Parameter;
 import study.component.Frame;
+import study.component.tab.account.operation.father.label.Label;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,26 +12,26 @@ import java.awt.event.MouseEvent;
 /**
  * @author Persolute
  * @version 1.0
- * @description 标签
+ * @description 表单父类
  * @email 1538520381@qq.com
- * @date 2024/3/1 17:02
+ * @date 2024/3/2 18:11
  */
-public class Label extends JLabel {
-    private final Frame frame;
+public class ItemFather extends JPanel {
+    protected final Frame frame;
 
-    public Label(Frame frame) {
+    public ItemFather(Frame frame, String text) {
         this.frame = frame;
 
-        setText("日期：");
-        setPreferredSize(new Dimension(Parameter.ACCOUNT_OPERATION_LABEL_WIDTH, Parameter.ACCOUNT_OPERATION_ITEM_HEIGHT));
-        setHorizontalAlignment(SwingConstants.RIGHT);
+        setPreferredSize(new Dimension(Parameter.FRAME_WIDTH / 2, Parameter.ACCOUNT_OPERATION_ITEM_HEIGHT));
 
-        addComponent();
+        addComponent(text);
         addListener();
     }
 
-    private void addComponent() {
+    private void addComponent(String text) {
+        setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 
+        add(new Label(frame, text));
     }
 
     private void addListener() {
